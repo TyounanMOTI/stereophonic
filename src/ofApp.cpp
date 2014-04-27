e18b32d4300abb691e08894fde9fc16c701e2ca2
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2014-04-27 14:30:14 +0900

    make file name of HRIR data

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,6 +1,7 @@
 #include "ofApp.h"
 
 #include <iostream>
+#include <boost/format.hpp>
 
 namespace {
     class AzimuthListGenerator_CIAIR
@@ -44,11 +45,13 @@ namespace {
 HRTFLoader::HRTFLoader(const std::string& path) : available_azimuth_(AzimuthListGenerator_CIAIR::NUM_DATA)
 {
     std::generate(available_azimuth_.begin(), available_azimuth_.end(), AzimuthListGenerator_CIAIR());
+
+    std::string filename_left_ear  = str(boost::format("%s/elev0/L0e%03da.dat") % path % 0);
 }
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    HRTFLoader loader("CIAIR/");
+    HRTFLoader loader("CIAIR");
 }
 
 //--------------------------------------------------------------
